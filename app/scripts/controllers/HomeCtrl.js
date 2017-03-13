69b171d3d9d3f0801331ffe933d34ab318f74f29
Brandon Caples <Brandon Caples>
2017-03-12 23:32:44 -0500

    Got stuck adding a modal

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,10 +1,17 @@
 (function() {
-    function HomeCtrl(Room) {
+    function HomeCtrl(Room, $uibModal) {
         this.rooms = Room;
+        this.addRoom = function() {
+            $uibModal.open({
+                controller: 'ModalCtrl as modal',
+                templateUrl: '/templates/modal.html',
+                size: 'sm'
+            });
+        }
     }
     
     angular
         .module('blocChat')
         .service('Room', HomeCtrl)
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
 })();
