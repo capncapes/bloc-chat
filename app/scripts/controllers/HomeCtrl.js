77acf6c20f8b9d27574d1c0806fc7b0b8c17ef24
Brandon Caples <Brandon Caples>
2017-06-22 12:26:28 -0500

    Added responsive styling

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -18,6 +18,15 @@
             $scope.activeId = room.$id;
             $scope.activeRoomName = room.name;
             $scope.currentActiveRoom = Message.getByRoomId(room.$id);
+            
+            if (screen.width < 800) {
+                $scope.sidebar = document.getElementById('sidebar');
+                $scope.sidebar.style.display = "none";
+                
+                document.body.style.backgroundImage = "url('../assets/images/bloc-chat-bg2b.jpg')";
+            } else {
+                document.body.style.backgroundImage = "url('../assets/images/bloc-chat-bg1b.jpg')";
+            }
         };
         
         // Check if there's an active room
@@ -32,6 +41,15 @@
         $scope.sendMessage = function(text) {
             var id = $scope.activeId;
             Message.send(text, id);
+        };
+        
+        this.toggleRooms = function() {
+            var sidebar = document.getElementById('sidebar');
+            if (sidebar.style.display === "none") {
+                sidebar.style.display = "inherit";
+            } else {
+                sidebar.style.display = "none"
+            }
         };
     }
     
